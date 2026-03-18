eb09a7c8450e7f136efb928c7ea8979e7f53b864
Zakwan Alam <122617017+zakwanalam@users.noreply.github.com>
2026-03-18 15:30:07 +0500

    Update API proxy URL to use HTTPS

--- website/vite.config.js
+++ website/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     proxy: {
-        '/api': 'backend-production-03e7c.up.railway.app'
+        '/api': 'https://backend-production-03e7c.up.railway.app'
     }
   },
   plugins: [react()],
